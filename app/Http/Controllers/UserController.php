cb642edde75b3c852c06f76359224df3c70b0993
Sabrina <sabrina@infranetworking.com>
2015-08-26 21:27:21 -0300

    Todo listo

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace nuevo\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use nuevo\Http\Requests;
+use nuevo\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    //
+}
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,21 @@
+@extends('templates.main')
+@section('title'){{'Welcome'}}@endsection
+@section('content')
+@include('templates.partials.header')
+<div class="row-fluid">
+  <div class="container">
+    <div class="col-md-2">
+
+    </div>
+    <div class="col-md-8">
+
+    </div>
+    <div class="col-md-2">
+
+    </div>
+  </div>
+</div>
+@endsection
+@section('footer')
+@include('templates.partials.footer')
+@stop
